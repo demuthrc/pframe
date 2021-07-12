384fede29cc342e3983982579bcfe84451a4c440
demuthrc <87095982+demuthrc@users.noreply.github.com>
2021-07-12 14:07:38 -0400

    Update picframe.py

--- app/picframe.py
+++ app/picframe.py
@@ -17,7 +17,7 @@ bootstrap = Bootstrap(app)
 
 def getImgs():
 	img_list=[]
-	for file in listdir('os.getcwd()+'/static/images/'):
+	for file in listdir(/images/'):
 		img_list.append(file)
 	return random.choice(img_list)
 
